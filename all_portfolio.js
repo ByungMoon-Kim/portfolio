ab9e82df748245f85743f565be5ef1f3dd0c78a3
Moon <darkyu457@naver.com>
2021-10-10 00:30:33 +0900

    10/9

--- all_portfolio.js
+++ all_portfolio.js
@@ -1,23 +1,13 @@
 const cgv = document.querySelector('.C-cgv');
-const cgvWorker = '\고객서비스 및 사이트 청결'
 const nara = document.querySelector('.C-nara');
 const habitus = document.querySelector('.C-ht');
 const dnd = document.querySelector('.C-DND');
 
-
 function cgvCareer(){
-    if(cgv){
-        cgv.innerText="2016.09.01 - 2019.09.28"
+        if(cgv){
+            cgv.innerText="2016.09.01 - 2019.09.28"
 }
 }
-// 이미지 원래대로 리턴
-function cgvCareer2(){
-    if(cgv){
-        cgv.image.src = './introdution_img/popcorn-icon.png'
-}
-}
-// 이미지 원래대로 리턴
-
 function naraCareer(){
     if(nara){
         nara.innerText="2020.07.16 - 2020.12.18";
@@ -33,8 +23,7 @@ function dndCareer(){
         dnd.innerText="2021.07.19 - ing"
     }
 }
-cgv.addEventListener('mouseenter', cgvCareer);
-cgv.addEventListener('mouseleave', cgvCareer2);
+cgv.addEventListener('mouseover', cgvCareer);
 nara.addEventListener('mouseover', naraCareer);
 habitus.addEventListener('mouseover', habitusCareer);
 dnd.addEventListener('mouseover', dndCareer);
